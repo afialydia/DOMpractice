b2719ce3114c30e1907d110b9f587ef11a9a5723
Afia-Lydia <you@example.com>
2019-09-04 10:05:28 -0400

    worked with getElementById and Query Selector

--- 3_3_project_files/js_sandbox/app.js
+++ 3_3_project_files/js_sandbox/app.js
@@ -1,39 +1,40 @@
 // document.getElementById()
 
-// console.log(document.getElementById('task-title'));
+console.log(document.getElementById('task-title'));
 
-// // Get things from the element
-// console.log(document.getElementById('task-title').id);
-// console.log(document.getElementById('task-title').className);
+//get things from element
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
 
-// const taskTitle = document.getElementById('task-title');
+//can use this in place of document.getelementbyid
+const taskTitle = document.getElementById('task-title')
 
-// // Change styling
-// taskTitle.style.background = '#333';
-// taskTitle.style.color = '#fff';
-// taskTitle.style.padding = '5px';
-// // taskTitle.style.display = 'none';
+//change styling
+document.getElementById('task-title').style.background = 'grey';
+document.getElementById('task-title').style.color = 'white';
+document.getElementById('task-title').style.padding = '8px';
+// document.getElementById('task-title').style.display = 'none';
 
-// // Change content
-// taskTitle.textContent = 'Task List';
-// taskTitle.innerText = 'My Tasks';
-// taskTitle.innerHTML = '<span style="color:red">Task List</span>';
 
-// document.querySelector()
+//change content
+document.getElementById('task-title').textContent = "Task List"
+document.getElementById('task-title').innerText = "My Tasks"
+document.getElementById('task-title').innerHTML = '<span style="color:purple">Task List</SPAN>';
+
+//document.querySelector()
 
 console.log(document.querySelector('#task-title'));
 console.log(document.querySelector('.card-title'));
 console.log(document.querySelector('h5'));
 
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
+document.querySelector('li').style.color = "red";
+document.querySelector('ul li').style.color = "blue";
 
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:nth-child(3)').style.color = 'yellow';
-document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
-
+document.querySelector('li:last-child').style.color = "green";
+document.querySelector('li:nth-child(3)').style.color = "hotpink";
+document.querySelector('li:nth-child(4)').textContent = "hotpink";
+document.querySelector('li:nth-child(odd)').style.background = "hotpink";
+document.querySelector('li:nth-child(even)').style.background = "orange";
 
 
 
