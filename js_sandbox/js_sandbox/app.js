efbe5166627f7057f2cac65f1f2db9f881afd702
Afia-Lydia <you@example.com>
2019-09-04 09:26:02 -0400

    practicing

--- js_sandbox/js_sandbox/app.js
+++ js_sandbox/js_sandbox/app.js
@@ -13,28 +13,15 @@ val = document.characterSet;
 val = document.contentType;
 
 val = document.forms;
-val = document.forms[0];
 val = document.forms[0].id;
 val = document.forms[0].method;
-val = document.forms[0].action;
-
 val = document.links;
-val = document.links[0];
 val = document.links[0].id;
 val = document.links[0].className;
 val = document.links[0].classList[0];
 
-val = document.images;
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+val = document.images
 
-let scripts = document.scripts;
-
-let scriptsArr = Array.from(scripts);
-
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute('src'));
-});
 
 console.log(val);
