bc30420fd5c7dd9b3996cf10f8cc5c6ca639d478
Afia-Lydia <you@example.com>
2019-09-04 10:38:12 -0400

    worked on multi element dom selectors

--- 3_4_project_files/js_sandbox/app.js
+++ 3_4_project_files/js_sandbox/app.js
@@ -1,51 +1,51 @@
-// document.getElementsByClassName
+// // document.getElementsByClassName
 
 // const items = document.getElementsByClassName('collection-item');
 // console.log(items);
 // console.log(items[0]);
 // items[0].style.color = 'red';
-// items[3].textContent = 'Hello';
+// items[3].textContent = 'HEEYYY';
 
 // const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// console.log(listItems)
 
-// console.log(listItems);
-
-// document.getElementsByTagName
-// let lis = document.getElementsByTagName('li');
+// // document.getElementsByTagName
+// let lis = document.getElementsByTagName('li')
 // console.log(lis);
 // console.log(lis[0]);
 // lis[0].style.color = 'red';
-// lis[3].textContent = 'Hello';
+// lis[3].textContent = 'HEEYYY';
 
-// // Conver HTML Collection into array
-// lis = Array.from(lis);
+
+// //convert HTML Collection into an array
+// lis = Array.from(lis)
 
 // lis.reverse();
 
-// lis.forEach(function(li, index){
+// lis.forEach(function (li, index) {
 //   console.log(li.className);
-//   li.textContent = `${index}: Hello`;
+//   li.textContent = `${index}: Hey Y'all`;
 // });
 
-// console.log(lis);
+// console.log(lis)
 
-// document.querySelectorAll
+//document.querySelectorAll
 const items = document.querySelectorAll('ul.collection li.collection-item');
-
-items.forEach(function(item, index){
-    item.textContent = `${index}: Hello`;
-});
+items.forEach(function (item, index) {
+    item.textContent = `${index}: Hey Y'all`;
+  });
 
 const liOdd = document.querySelectorAll('li:nth-child(odd)');
 const liEven = document.querySelectorAll('li:nth-child(even)');
 
-liOdd.forEach(function(li, index){
-  li.style.background = '#ccc';
+
+liOdd.forEach(function(li,index){
+  li.style.background = 'pink';
 });
 
 for(let i = 0; i < liEven.length; i++){
-  liEven[i].style.background = '#f4f4f4';
+  liEven[i].style.background = 'cyan';
 }
+  console.log(items);
 
-
-console.log(items);
+  
